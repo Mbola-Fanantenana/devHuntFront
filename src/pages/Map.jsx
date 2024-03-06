952461b3ce144feca5ee1ab4fd215ba4ca6ea830
Mbola Ramanandraibe <mbola.ram26@gmail.com>
2024-03-06 17:09:41 +0300

    trajet map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import * as L from "leaflet";
-import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder";
 
 function Map() {
   const map = useRef(null);
@@ -16,11 +20,31 @@ function Map() {
       }).addTo(map.current);
     }
 
-    // Add a marker to the map
-    const marker = L.marker([-21.4551733,47.0931990], 19.05).addTo(map.current);
+    // Marquer de position de l'ENI
+    const automaticMarker = L.marker([-21.4551733, 47.093199], 19.5).addTo(
+      map.current
+    );
     // popup
-    marker.bindPopup("Ecole Nationale d'Informatique!").openPopup();
-  
+    automaticMarker.bindPopup("Ecole Nationale d'Informatique").openPopup();
+
+    //geolocation API pour avoir la position actuelle
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      // Marquer du position actuelle
+      const userMarker = L.marker([latitude, longitude]).addTo(map.current);
+      // Popup
+      userMarker.bindPopup("Votre position actuelle").openPopup();
+
+      // Creation du trajet entre eni et position actuelle
+      L.Routing.control({
+        waypoints: [
+          L.latLng(automaticMarker.getLatLng()), // coordonnées de l'eni
+          L.latLng(userMarker.getLatLng()), // coordonnées de la position actuelle
+        ],
+      }).addTo(map.current);
+
+      map.current.panTo([latitude, longitude]);
+    });
   }, []);
 
   return <div id="mapId" style={{ height: "100vh", width: "100vw" }}></div>;
