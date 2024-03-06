8fa9710915873eb7053ba1b8d5802f0ba566a898
Hasina-Tiana <hasinatianarah@gmail.com>
2024-03-06 23:42:44 +0300

    modification thème

--- src/pages/Authentification.jsx
+++ src/pages/Authentification.jsx
@@ -78,10 +78,10 @@ const Authentification = () => {
     }
 
     return (
-        <div className="bg-gradient-to-tl from-blue-800 to-emerald-500 min-h-screen flex flex-col">
+        <div className="bg-[#ddd] min-h-screen flex flex-col">
             <div className="flex-grow flex justify-center items-center">
                 <div className='relative md:w-[40%] sm:w-[40%]'>
-                    <p className="absolute flex items-center xl:text-4xl sm:block xs:block mt-10 text-white text-opacity-50 backdrop-blur-lg">Découvrez la technologie différemment.</p>
+                    <p className="absolute flex items-center xl:text-4xl sm:block xs:block mt-10 text-[#323232] text-opacity-80 backdrop-blur-lg">Découvrez la technologie différemment.</p>
                     <div className={`relative xl:w-[100%] lg:w-[100%] md:w-[100%] sm:block xs:block`}>
                         <Lottie
                             animationData={LoginAnimation}
@@ -90,16 +90,16 @@ const Authentification = () => {
                             className="w-full h-auto"
                         />
                     </div>
-                    <p className="absolute flex items-center xl:text-4xl sm:block -mt-20 text-white text-opacity-50 backdrop-blur-lg">Au-delà de votre imagination.</p>
+                    <p className="absolute flex items-center xl:text-4xl sm:block -mt-20 text-[#323232] text-opacity-80 backdrop-blur-lg">Au-delà de votre imagination.</p>
                 </div>
-                <div className="md:bg-white md:bg-opacity-20 sm:bg-transparent backdrop-blur-lg rounded p-8 max-w-md relative w-full md:w-auto border border-gray-200">
-                    <h2 className="text-3xl font-bold text-white mb-4">Connectez-vous</h2>
-                    <hr className="border-t border-white border-opacity-50 mb-2" />
+                <div className="md:bg-white md:bg-opacity-80 sm:bg-transparent backdrop-blur-lg rounded p-8 max-w-md relative w-full md:w-auto border border-gray-200">
+                    <h2 className="text-3xl font-bold text-[#323232] mb-4">Connectez-vous</h2>
+                    <hr className="border-t border-[#323232] border-opacity-50 mb-2" />
                     <form>
                         <div className="mb-4">
-                            <label htmlFor="email" className="text-white block mb-1">Nom d&#39;utilisateur</label>
+                            <label htmlFor="email" className="text-[#323232] block mb-1">Nom d&#39;utilisateur</label>
                             <input
-                                className="w-full px-4 py-2 rounded-md bg-white bg-opacity-50 focus:outline-none focus:bg-opacity-75"
+                                className="w-full px-4 py-2 rounded bg-slate-300 bg-opacity-50 focus:outline-none focus:bg-opacity-80"
                                 name="pseudoUtilisateur"
                                 id="email"
                                 type="text"
@@ -108,11 +108,11 @@ const Authentification = () => {
                             />
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="password" className="text-white block mb-1">Mot de passe</label>
+                            <label htmlFor="password" className="text-[#323232] block mb-1">Mot de passe</label>
                             <div className="relative">
                                 <input
                                     type={showPassword ? "text" : "password"}
-                                    className="w-full px-4 py-2 rounded-md bg-white bg-opacity-50 focus:outline-none focus:bg-opacity-75"
+                                    className="w-full px-4 py-2 rounded bg-slate-300 bg-opacity-50 focus:outline-none focus:bg-opacity-80"
                                     name="mdpUtilisateur"
                                     id="password"
                                     value={formData.mdpUtilisateur}
@@ -124,20 +124,20 @@ const Authentification = () => {
                                     onClick={togglePasswordVisibility}
                                     className="absolute inset-y-0 right-0 flex items-center pr-2"
                                 >
-                                    {showPassword ? <FiEyeOff className="text-white" /> : <FiEye className="text-white" />}
+                                    {showPassword ? <FiEye className="text-[#323232]" /> : <FiEyeOff className="text-[#323232]" />}
                                 </button>
                             </div>
                         </div>
                         <button
                             onClick={(e) => handleSubmit(e)}
                             type="submit"
-                            className="bg-white w-full bg-opacity-25 hover:bg-opacity-50 text-white py-2 px-4 rounded-md focus:outline-none text-lg font-medium"
+                            className="bg-[#007a55] w-full bg-opacity-60 hover:bg-opacity-80 text-white py-2 px-4 rounded focus:outline-none text-lg font-medium"
                             disabled={loading}
                         >
                             {loading ? 'Chargement...' : 'Se connecter'}
                         </button>
                     </form>
-                    <p className="mt-4 text-white">Vous n&#39;avez pas de compte ? <Link to="/sign-up" className="text-blue-300 hover:text-blue-400">Inscrivez-vous</Link></p>
+                    <p className="mt-4 text-[#323232]">Vous n&#39;avez pas de compte ? <Link to="/sign-up" className="text-[#007a55] hover:text-emerald-700">Inscrivez-vous</Link></p>
                 </div>
                 <ErrorModal isOpen={error !== null} onClose={handleCloseErrorModal} titleMessage="Message d'erreur">
                     {error}
@@ -146,7 +146,7 @@ const Authentification = () => {
                     <Toast message="Connecter avec succès !" />
                 )}
             </div>
-            <Footer textColor="#eee" copyright="&copy; < Minds Merge /> - 2024" />
+            <Footer textColor="#323232" copyright="&copy; < Minds Merge /> - 2024" />
         </div >
     );
 }
