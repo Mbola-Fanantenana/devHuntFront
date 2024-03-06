8511814b03687b95b876f34cbd546f13936b9ab7
Mbola Ramanandraibe <mbola.ram26@gmail.com>
2024-03-06 16:52:05 +0300

    map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
-import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-import "leaflet-routing-machine";
-import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-import "leaflet-control-geocoder/dist/Control.Geocoder";
 
 function Map() {
   const map = useRef(null);
@@ -20,25 +16,11 @@ function Map() {
       }).addTo(map.current);
     }
 
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
+    // Add a marker to the map
+    const marker = L.marker([-21.4551733,47.0931990], 19.05).addTo(map.current);
+    // popup
+    marker.bindPopup("Ecole Nationale d'Informatique!").openPopup();
   
-          // Add a marker for the user's current location 
-          const userMarker = L.marker([latitude, longitude]).addTo(map.current);
-          // Popup for the user's marker
-          userMarker.bindPopup("Your Current Location").openPopup();
-  
-          // Add a marker to the map (e.g., Ecole Nationale d'Informatique)
-          const marker = L.marker([-21.4551733, 47.093199], 19.05).addTo(map.current);
-          // Popup for the marker
-          marker.bindPopup("Ecole Nationale d'Informatique!").openPopup();
-        },
-        (error) => {
-          console.error("Error getting user's location:", error);
-        }
-      );
-
   }, []);
 
   return <div id="mapId" style={{ height: "100vh", width: "100vw" }}></div>;
