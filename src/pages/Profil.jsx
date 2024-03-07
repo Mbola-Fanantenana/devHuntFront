f1e613070470ee1d448b45a0318337508a06794d
Mbola Ramanandraibe <mbola.ram26@gmail.com>
2024-03-07 08:02:12 +0300

    profil user

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,152 +1,243 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import avatar from "../assets/Image/avatar.jpg";
+import config from "../../config.json";
+import axios from "axios";
 
-const Profil = ({ userId }) => {
-  const [user, setUser] = useState({
-    nomUtilisateur: '',
-    prenomUtilisateur: '',
-    adresseUtilisateur: '',
-    telUtilisateur: '',
-    niveauUtilisateur: '',
-    matriculeUtilisateur: '',
-    pseudoUtilisateur: '',
-    emailUtilisateur: '',
-    imgUtilisateur: '',
-    mdpUtilisateur: '',
-  });
+const Profil = () => {
+  const [user, setUser] = useState([]);
+  const [userConnected, setUserConnected] = useState("");
 
   useEffect(() => {
-    // Charger les informations de l'utilisateur à partir de l'API
-    const fetchUserData = async () => {
+    const userLocaleStorage = localStorage.getItem("userSession");
+    setUserConnected(JSON.parse(userLocaleStorage).idUtilisateur);
+  }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
       try {
-        const response = await axios.get(`/api/user/${userId}`);
+        const response = await axios.get(
+          `${config.API_HOST}/api/user/${userConnected}`
+        );
         setUser(response.data);
+        console.log(response.data);
       } catch (error) {
-        console.error('Erreur lors du chargement des données utilisateur', error);
+        console.error(
+          "Erreur lors de la récupération des données utilisateur",
+          error
+        );
       }
     };
 
-    fetchUserData();
-  }, [userId]);
+    fetchUser();
+  }, [userConnected]);
 
   const handleInputChange = (e) => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
   };
 
-  const handleUpdateUser = async () => {
-    try {
-      // Effectuer la mise à jour à partir de l'API
-      await axios.put(`/api/updateUser/${userId}`, user);
-      // Mettre à jour l'état local ou rediriger vers la page du profil
-    } catch (error) {
-      console.error('Erreur lors de la mise à jour de l\'utilisateur', error);
-    }
+  const handleSubmit = () => {
+    axios
+      .patch(`${config.API_HOST}/api/updateUser/${userConnected}`, user, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Mise à jour réussie", response.data);
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la mise à jour des données utilisateur",
+          error
+        );
+      });
   };
 
   return (
-    <div className="max-w-md p-6 mx-auto mt-10 overflow-hidden bg-white rounded-md shadow-md profil-container">
-      <h2 className="mb-4 text-2xl font-bold">Mon Profil</h2>
-      <form>
-        <div className="mb-4">
-          <label className="block text-gray-600">Nom :</label>
-          <input
-            type="text"
-            name="nomUtilisateur"
-            value={user.nomUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+    <div className="px-6 py-24 bg-white isolate sm:py-14 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" />
+      </div>
+      <div className="flex items-center justify-center max-w-2xl mx-auto">
+        <div className="w-32 h-32 border-2 rounded-full">
+          <img
+            className="object-cover w-full h-full"
+            src={avatar}
+            alt="Photo de profil"
           />
         </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Prénom :</label>
-          <input
-            type="text"
-            name="prenomUtilisateur"
-            value={user.prenomUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
+      </div>
+      <form onSubmit={handleSubmit} className="max-w-xl mx-auto mt-16 sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Prénom
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="prenomUtilisateur"
+                value={user.prenomUtilisateur}
+                id="prenomUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Nom
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="nomUtilisateur"
+                value={user.nomUtilisateur}
+                id="nomUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Téléphone
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="telUtilisateur"
+                value={user.telUtilisateur}
+                id="telUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Niveau
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="niveauUtilisateur"
+                value={user.niveauUtilisateur}
+                id="niveauUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Pseudo
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="pseudoUtilisateur"
+                value={user.pseudoUtilisateur}
+                id="pseudoUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Numero matricule
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="matriculeUtilisateur"
+                value={user.matriculeUtilisateur}
+                id="matriculeUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="company"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Adresse
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="adresseUtilisateur"
+                value={user.adresseUtilisateur}
+                id="adresseUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="organization"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="emailUtilisateur"
+                value={user.emailUtilisateur}
+                id="emailUtilisateur"
+                onChange={handleInputChange}
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
         </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Adresse :</label>
-          <input
-            type="text"
-            name="adresseUtilisateur"
-            value={user.adresseUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Enregister les modifications
+          </button>
         </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Téléphone :</label>
-          <input
-            type="text"
-            name="telUtilisateur"
-            value={user.telUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Niveau :</label>
-          <input
-            type="text"
-            name="niveauUtilisateur"
-            value={user.niveauUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Matricule :</label>
-          <input
-            type="text"
-            name="matriculeUtilisateur"
-            value={user.matriculeUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Pseudo :</label>
-          <input
-            type="text"
-            name="pseudoUtilisateur"
-            value={user.pseudoUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600">Email :</label>
-          <input
-            type="text"
-            name="emailUtilisateur"
-            value={user.emailUtilisateur}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        {/* Ajoutez d'autres champs ici avec le même format */}
-        
-        <button
-          type="button"
-          onClick={handleUpdateUser}
-          className="px-4 py-2 text-white bg-blue-500 rounded-md focus:outline-none"
-        >
-          Enregistrer les modifications
-        </button>
       </form>
     </div>
   );
